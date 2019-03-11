c25420137a4a548cdf0ea39f2f52c7c56bce4eb4
Tom Kazatel <kazatel@jiktogroup.cz>
2019-03-11 10:59:14 +0100

    fix countdown on safari

--- default.js
+++ default.js
@@ -1,65 +1,65 @@
 function countdown() {
-  // setups
-  var miliToDays = 86400000;
-  var miliToHour = 3600000;
-  var miliToMinutes = 60000;
-  var days = 0;
-  var hours = 0;
-  var minutes = 0;
+    // setups
+    var miliToDays = 86400000;
+    var miliToHour = 3600000;
+    var miliToMinutes = 60000;
+    var days = 0;
+    var hours = 0;
+    var minutes = 0;
 
-  // get diff
-  var wedding = new Date("2019-05-18 11:30:00");
-  var now = new Date();
-  var diff = wedding.getTime() - now.getTime();
+    // get diff
+    var wedding = new Date('2019/05/18 11:30:00');
+    var now = new Date();
+    var diff = wedding.getTime() - now.getTime();
 
-  if (diff >= 0) {
-    // get days
-    days = Math.floor(diff / miliToDays);
-    diff = diff - days * miliToDays;
+    if (diff >= 0) {
+        // get days
+        days = Math.floor(diff / miliToDays);
+        diff = diff - days * miliToDays;
 
-    // get hours
-    hours = Math.floor(diff / miliToHour);
-    diff = diff - hours * miliToHour;
+        // get hours
+        hours = Math.floor(diff / miliToHour);
+        diff = diff - hours * miliToHour;
 
-    // get minutes
-    minutes = Math.floor(diff / miliToMinutes);
-    diff = diff - minutes * miliToMinutes;
-  }
+        // get minutes
+        minutes = Math.floor(diff / miliToMinutes);
+        diff = diff - minutes * miliToMinutes;
+    }
 
-  // assing values to labels
-  document.getElementById("days").innerHTML = days;
-  document.getElementById("hours").innerHTML = hours;
-  document.getElementById("minutes").innerHTML = minutes;
+    // assing values to labels
+    document.getElementById('days').innerHTML = days;
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('minutes').innerHTML = minutes;
 }
 
 function handleQR() {
-  var price = document.getElementById("price").value;
-  var message = document.getElementById("note").value;
-  var date = new Date();
-  var formatDate = date.getFullYear() + date.getMonth() + date.getDate();
+    var price = document.getElementById('price').value;
+    var message = document.getElementById('note').value;
+    var date = new Date();
+    var formatDate = date.getFullYear() + date.getMonth() + date.getDate();
 
-  var hash =
-    "SPD*1.0*ACC:CZ0961000000001025000263*AM:" +
-    price +
-    "*CC:CZK*DT:" +
-    formatDate +
-    "*MSG:" +
-    message;
+    var hash =
+        'SPD*1.0*ACC:CZ0961000000001025000263*AM:' +
+        price +
+        '*CC:CZK*DT:' +
+        formatDate +
+        '*MSG:' +
+        message;
 
-  var url = "https://chart.googleapis.com/chart?cht=qr&chs=180x180&chl=" + hash;
-  var image = document.createElement("img");
-  image.src = url;
-  image.alt = "qr code";
+    var url = 'https://chart.googleapis.com/chart?cht=qr&chs=180x180&chl=' + hash;
+    var image = document.createElement('img');
+    image.src = url;
+    image.alt = 'qr code';
 
-  var form = document.getElementById("form");
-  var parentNode = form.parentNode;
+    var form = document.getElementById('form');
+    var parentNode = form.parentNode;
 
-  parentNode.replaceChild(image, form);
+    parentNode.replaceChild(image, form);
 
-  return false;
+    return false;
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  countdown();
-  setInterval(countdown, 30000);
+document.addEventListener('DOMContentLoaded', function(event) {
+    countdown();
+    setInterval(countdown, 30000);
 });
